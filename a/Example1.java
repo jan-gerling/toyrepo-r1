e8895b22847c7c54a9e187f9f674db274e6bc103
Mauricio Aniche <mauricioaniche@gmail.com>
2019-08-24 13:56:59 +0200

    extract method (not detected by rm)

--- a/Example1.java
+++ a/Example1.java
@@ -2,9 +2,14 @@ package a;
 
 class Example1 {
 
+	private int convert(int a) {
+		int b = a + 5;
+		return b;
+	}
+
 	public void m1() {
 		int a = 0;
-		int b = a + 5;
+		int b = convert(a);
 
 		b = b * 2;
 
